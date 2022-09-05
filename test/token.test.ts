105d58e33f02c3a9c3291c1e74046507bfe72c8e
Lucacez <lucacespedes@protonmail.com>
2022-09-05 15:03:50 -0300

    important flow tested

--- test/token.test.ts
+++ test/token.test.ts
@@ -2,24 +2,60 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { ETHPool__factory } from "../typechain-types";
 
-describe("Token", function () {
-  
-  beforeEach(async function(){
-    [this.deployer, this.alice, this.bob] = await ethers.getSigners();
+describe("ETHPool", function () {
 
-    this.ETHPool = await new ETHPool__factory(this.deployer).deploy();
+  const ADMIN_ROLE = "0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775";
+  const TEAM_ROLE = "0x5146a08baf902532d0ee2f909971144f12ca32651cd70cbee1117cddfb3b3b33";
 
-    this.ETHPool = await ethers.getContractFactory("ETHPool");
+  beforeEach(async function () {
+    [this.deployer, this.alice, this.bob, this.member] = await ethers.getSigners();
 
-    this.ethpool = await this.ETHPool.deploy();
+    this.ethpool = await new ETHPool__factory(this.deployer).deploy();
 
     await this.ethpool.deployed();
-  }
-  
-  describe("init", function () {
-
-    it("anda", async function () {
-      expect(await this.ethpool.connect(this.deployer).getPoolBalance()).to.be.eq(0);
-    })
   })
+
+  describe("#init", function () {
+    it('alice can deposit and withdraw your ether', async function () {
+      
+      await this.ethpool.connect(this.alice).deposit({ value: ethers.utils.parseEther("0.5") });
+      expect(await this.ethpool.connect(this.alice).getPoolBalance()).to.be.eq(ethers.utils.parseEther("0.5"));
+
+      await this.ethpool.connect(this.alice).withdrawExactETH(ethers.utils.parseEther("0.2"));
+      expect(await this.ethpool.connect(this.alice).getUserBalance(this.alice.address)).to.be.eq(ethers.utils.parseEther("0.3"));
+
+      await this.ethpool.connect(this.alice).withdrawAllETH();
+      expect(await this.ethpool.connect(this.alice).getPoolBalance()).to.be.eq(0);
+    })
+
+    it('admin can deposit rewards for the users in the pool', async function () {
+
+      await this.ethpool.connect(this.alice).deposit({ value: ethers.utils.parseEther("0.5") });
+
+      await this.ethpool.connect(this.deployer).grantRole(TEAM_ROLE,this.bob.address);
+      expect(await this.ethpool.hasRole(TEAM_ROLE, this.bob.address)).to.be.eq(true);
+
+      expect(await this.ethpool.connect(this.bob).depositRewards({ value: ethers.utils.parseEther("0.5") }));
+      //expect(await this.ethpool.connect(this.deployer).depositRewards({ value: ethers.utils.parseEther("1") })).to.be.reverted;
+
+      expect(await this.ethpool.getRewardBalance()).to.be.eq(ethers.utils.parseEther("0.5"));
+    });
+  });
+
+  describe("#flow important", async function () {
+    it('', async function () {
+      await this.ethpool.connect(this.deployer).grantRole(TEAM_ROLE,this.member.address);
+      expect(await this.ethpool.hasRole(TEAM_ROLE, this.member.address)).to.be.eq(true);
+
+      await this.ethpool.connect(this.alice).deposit({ value: ethers.utils.parseEther("0.01") });
+      await this.ethpool.connect(this.bob).deposit({ value: ethers.utils.parseEther("0.03") });
+      expect(await this.ethpool.getPoolBalance()).to.be.eq(ethers.utils.parseEther("0.04"));
+      
+      await this.ethpool.connect(this.member).depositRewards({ value: ethers.utils.parseEther("0.02") });
+      await this.ethpool.connect(this.alice).claimRewards();
+      await this.ethpool.connect(this.bob).claimRewards();
+      expect(await this.ethpool.getUserBalance(this.alice.address)).to.be.eq(ethers.utils.parseEther("0.015"));
+      expect(await this.ethpool.getUserBalance(this.bob.address)).to.be.eq(ethers.utils.parseEther("0.045"));
+    });
+  });
 });
